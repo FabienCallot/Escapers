313ed91aed69a7e126ac02024ce83c30f7d0e726
FabienCallot <fab.callot@gmail.com>
2021-11-07 16:14:53 +0100

    ajout d'un partial , refactor, commentaire et supression d'un module

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3010;
-
-// import de mes modules
 const rooms = require('./my modules/rooms');
-const currentRoom = require('./my modules/currentRoom');
-
 //je définis le template motor
 app.set('view engine', 'ejs');
 app.set('views' , './views');
@@ -15,13 +11,12 @@ app.use(express.static('public'));
 
 //route principale 
 app.get('/' , (req , res) => {
-
     res.render('index' , {
         title : 'Escapers Lille',
         name : 'Escapers, time to escape.'
     })
-
 });
+// route dynamique pour mes rooms
 app.get('/:room' , (req , res) => {
     let currentRoom = null;
     for(const room of rooms) {
@@ -29,15 +24,19 @@ app.get('/:room' , (req , res) => {
             currentRoom = room;
         };
     };
-      res.render(`${currentRoom.title}` , {
-          title : `${currentRoom.title}`,
-          name : `${currentRoom.name}`
-      }) 
+    if(currentRoom) {
+        res.render(`${currentRoom.title}` , {
+        title : `${currentRoom.title}`,
+        name : `${currentRoom.name}`
+    });
+    } else {
+        res.status(404).send("404 - page not found");
+    }; 
 });
-
-
+//404 pour toute les autres page non trouvées
+app.get('*' , (req , res) => {
+    res.status(404).send('404 - page not found');
+});
 app.listen(PORT , () => {
     console.log('server available on http://localhost:3010');
 });
-
-console.log(rooms);
