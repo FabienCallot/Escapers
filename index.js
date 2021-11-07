ab05cbb0b7c2dbc0e55b4951de728c84b73ea404
FabienCallot <fab.callot@gmail.com>
2021-11-07 15:37:20 +0100

    ajout d'un module room, creation route dynamique pour les rooms, tittle & name dynamique

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3010;
 
+// import de mes modules
+const rooms = require('./my modules/rooms');
+const currentRoom = require('./my modules/currentRoom');
+
 //je définis le template motor
 app.set('view engine', 'ejs');
 app.set('views' , './views');
@@ -11,9 +15,29 @@ app.use(express.static('public'));
 
 //route principale 
 app.get('/' , (req , res) => {
-    res.render('index')
-})
+
+    res.render('index' , {
+        title : 'Escapers Lille',
+        name : 'Escapers, time to escape.'
+    })
+
+});
+app.get('/:room' , (req , res) => {
+    let currentRoom = null;
+    for(const room of rooms) {
+        if(room.title.toLowerCase() === req.params.room.toLowerCase()){
+            currentRoom = room;
+        };
+    };
+      res.render(`${currentRoom.title}` , {
+          title : `${currentRoom.title}`,
+          name : `${currentRoom.name}`
+      }) 
+});
+
 
 app.listen(PORT , () => {
     console.log('server available on http://localhost:3010');
-})
+});
+
+console.log(rooms);
---
+++ my modules/rooms.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        'title': "Escapers-Braquage",
+        'name': "Le braquage à la Lilloise"
+    },
+    {
+        'title': "Escapers-Magot",
+        'name': "Le Magot de Mémé"
+    },
+    {
+        'title': "Escapers-Loup",
+        'name': "Le grand méchant loup"
+    },
+];
