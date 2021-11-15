2e502b14b0fc816519bbb66a31620781db8f0e56
FabienCallot <fab.callot@gmail.com>
2021-11-15 14:59:57 +0100

    ajout eslint & text intro sur pas acceuil seulement

--- app/router.js
+++ app/router.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const rooms = require('./my modules/rooms.json');
-//route principale 
-router.get('/' , (req , res) => {
-    res.render('index' , {
-        title : 'Escapers Lille',
-        name : 'Escapers, time to escape.',
-        intro : `test`
-    })
+//route principale
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Escapers Lille',
+        name: 'Escapers, time to escape.',
+        intro1: `Fouille, communication et résolution d'énigmes vous permettrons
+        d'avancer dans un décors et une imersion total.`,
+        intro2: `Vos équipe est votre force! Ou votre faiblesse... Venez relever le
+        challenge et essayer de vous échapper en moins d'une heure !`,
+       
+    });
 });
 // route dynamique pour mes rooms
-router.get('/:room' , (req , res) => {
+router.get('/:room', (req, res) => {
     let currentRoom = null;
-    for(const room of rooms) {
-        if(room.title.toLowerCase() === req.params.room.toLowerCase()){
+    for (const room of rooms) {
+        if (room.title.toLowerCase() === req.params.room.toLowerCase()) {
             currentRoom = room;
-        };
-    };
-    if(currentRoom) {
-        res.render(`${currentRoom.title}` , {
-        title : `${currentRoom.title}`,
-        name : `${currentRoom.name}`
-    });
+        }
+    }
+    if (currentRoom) {
+        res.render(`${currentRoom.title}`, {
+            title: `${currentRoom.title}`,
+            name: `${currentRoom.name}`,
+            intro1: ``,
+            intro2: ``
+        });
     } else {
-        res.status(404).send("404 - page not found");
-    }; 
+        res.status(404).send('404 - page not found');
+    }
 });
 //404 pour toute les autres page non trouvées
-router.get('*' , (req , res) => {
+router.get('*', (req, res) => {
     res.status(404).send('404 - page not found');
 });
 
--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const PORT = 3010;
 const router = require('./app/router');
 //je définis le template motor
 app.set('view engine', 'ejs');
-app.set('views' , __dirname + '/app/views');
+app.set('views', __dirname + '/app/views');
 // je définis le dossier public comme static et accessible pour le client
-app.use(express.static( __dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 // j'utlise le router pour mes routes
 app.use(router);
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log('server available on http://localhost:3010');
 });
-
-
